29ef6e6a15f779bea914455fa517a7d3efc5668d
JvtrL <joaovitorlimadasilveira@gmail.com>
2025-08-28 21:08:16 -0300

    first commit

---
+++ src/Cantina.java
@@ -0,0 +1,29 @@
+public class Cantina {
+
+    String nome;
+    Salgado [] salgados = new Salgado[10];
+
+    void addSalgado (Salgado novoSalgado)
+    {
+        for(int i=0;i<salgados.length;i++)
+        {
+            if (salgados[i] == null)
+            {
+                salgados[i] = novoSalgado;
+            }
+            break;
+        }
+    }
+
+    void mostraInfo()
+    {
+        for(Salgado s : salgados)
+        {
+            if(s == null)
+            {
+                System.out.println(s.nome);
+            }
+        }
+    }
+
+}
